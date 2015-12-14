27584775f4df07dc7eb60fbaa158e244d6abd004
jimmyqian <jimmyqian1986@163.com>
2015-12-14 17:21:54 +0800

    20151214 迭代器的使用

--- JimmyTest/src/com/jimmy/hsp/class1/Demo8.java
+++ JimmyTest/src/com/jimmy/hsp/class1/Demo8.java
@@ -4,17 +4,28 @@
 package com.jimmy.hsp.class1;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Vector;
+
+import jimmy.commons.Log4j;
+
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author jimmy
  *
  */
 public class Demo8 {
-	LinkedList<Employer> linkedlist = new LinkedList<Employer>();
+	static LinkedList<Employer> linkedlist = new LinkedList<Employer>();
 	Vector<Employer> vector = new Vector<Employer>();
 	HashMap hs = new HashMap();
+	Hashtable ht = new Hashtable();
+	HashSet<Employer> hset = new HashSet<Employer>();
+	static Log4j log4j = new Log4j();
+	static Logger logger = log4j.getLogger(Demo8.class.getName());
 
 	/**
 	 * @param args
@@ -22,16 +33,26 @@ public class Demo8 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Demo8 dm8 = new Demo8();
+		// logger.info(System.currentTimeMillis());
 		for (int i = 0; i < 5; i++) {
 			dm8.linkListInsertEmp(i + "", "name" + i, (i * 100) / 12f);
 			dm8.vectorInsertEmp(i + "", "name" + i, (i * 100) / 12f);
 			dm8.hashMapInsertEmp(i + "", "name" + i, (i * 100) / 12f);
+			dm8.hashTableInsertEmp(i + "", "name" + i, (i * 100) / 12f);
+			dm8.hashSetInsertEmp(i + "", "name" + i, (i * 100) / 12f);
 		}
 		dm8.linkListGetEmp();
-		System.out.println("-=================");
+		logger.info("======LINKEDLIST========");
 		dm8.vectorGetEmp();
-		System.out.println("------------------");
+		logger.info("---------VECTOR---------");
 		dm8.hashMapGetEmp();
+		logger.info("=======HASHMAP======");
+		dm8.hashTableGetEmp();
+		logger.info("=======HASHTABLE====");
+		dm8.hashSetGetEmp();
+		logger.info("=======HASHSET======");
+		dm8.iteratorGetLinkedList(linkedlist);
+
 	}
 
 	/**
@@ -42,7 +63,7 @@ public class Demo8 {
 		Employer emp = null;
 		for (int i = 0; i < linkedlist.size(); i++) {
 			emp = linkedlist.get(i);
-			System.out.println(emp.getEmpName() + " " + emp.getEmpSal());
+			logger.info(emp.getEmpName() + " " + emp.getEmpSal());
 		}
 	}
 
@@ -63,7 +84,7 @@ public class Demo8 {
 		Employer emp = null;
 		for (int i = 0; i < vector.size(); i++) {
 			emp = vector.get(i);
-			System.out.println(emp.getEmpName() + " " + emp.getEmpSal());
+			logger.info(emp.getEmpName() + " " + emp.getEmpSal());
 		}
 	}
 
@@ -77,14 +98,15 @@ public class Demo8 {
 	}
 
 	/**
-	 * VECTOR 集合
+	 * HashMap 集合
 	 */
 	private void hashMapGetEmp() {
 		// TODO Auto-generated method stub
 		Employer emp = null;
-		for (int i = 0; i < hs.size(); i++) {
-			emp = (Employer) hs.get(i);
-			System.out.println(emp.getEmpName() + " " + emp.getEmpSal());
+		for (int i = 1; i < hs.size(); i++) {
+			emp = (Employer) hs.get(i + "");
+			// logger.info(hs.size());
+			logger.info(emp.getEmpName() + " " + emp.getEmpSal());
 		}
 	}
 
@@ -95,5 +117,61 @@ public class Demo8 {
 		// TODO Auto-generated method stub
 		Employer emp = new Employer(empNo, empName, empSal);
 		hs.put(empNo, emp);
+	}
+
+	/**
+	 * HashMap 集合
+	 */
+	private void hashTableGetEmp() {
+		// TODO Auto-generated method stub
+		Employer emp = null;
+		for (Iterator it = ht.keySet().iterator(); it.hasNext();) {
+			String key = it.next().toString();
+			emp = (Employer) ht.get(key);
+			// logger.info(hs.size());
+			logger.info(emp.getEmpName() + " " + emp.getEmpSal());
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private void hashTableInsertEmp(String empNo, String empName, float empSal) {
+		// TODO Auto-generated method stub
+		Employer emp = new Employer(empNo, empName, empSal);
+		ht.put(empNo, emp);
+	}
+
+	private void hashSetGetEmp() {
+		// TODO Auto-generated method stub
+		Employer emp = null;
+		Object[] al = hset.toArray();
+		for (int i = 0; i < al.length; i++) {
+			emp = (Employer) al[i];
+			// logger.info(hs.size());
+			logger.info(emp.getEmpName() + " " + emp.getEmpSal());
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private void hashSetInsertEmp(String empNo, String empName, float empSal) {
+		// TODO Auto-generated method stub
+		Employer emp = new Employer(empNo, empName, empSal);
+		hset.add(emp);
+	}
+
+	/**
+	 * 使用迭代器获取信息
+	 * 
+	 * @param linlist
+	 */
+	private void iteratorGetLinkedList(LinkedList linlist) {
+		for (Iterator it = linlist.iterator(); it.hasNext();) {
+			Employer emp = (Employer) it.next();
+			logger.info("use Iterator to get :" + emp.getEmpName() + " "
+					+ emp.getEmpSal());
+		}
 	}
 }
--- JimmyTest/src/jimmy/commons/ConnectionFactory.java
+++ JimmyTest/src/jimmy/commons/ConnectionFactory.java
@@ -6,53 +6,66 @@ package jimmy.commons;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
- 
+
 import javax.sql.DataSource;
- 
+
 import org.apache.commons.dbcp.DriverManagerConnectionFactory;
 import org.apache.commons.dbcp.PoolableConnection;
 import org.apache.commons.dbcp.PoolableConnectionFactory;
 import org.apache.commons.dbcp.PoolingDataSource;
 import org.apache.commons.pool.impl.GenericObjectPool;
- 
-public class ConnectionFactory {
-	
-    private static interface Singleton {
-       
-    }
- 
-    private final DataSource dataSource;
- 
-    private ConnectionFactory(String username,String passwd,String url) {
-        Properties properties = new Properties();
-        properties.setProperty("user", username);
-        properties.setProperty("password", passwd); // or get properties from some configuration file
- 
-        GenericObjectPool<PoolableConnection> pool = new GenericObjectPool<PoolableConnection>();
-        DriverManagerConnectionFactory connectionFactory = new DriverManagerConnectionFactory(
-                url, properties
-        );
-        new PoolableConnectionFactory(
-                connectionFactory, pool, null, "SELECT 1", 3, false, false, Connection.TRANSACTION_READ_COMMITTED
-        );
- 
-        this.dataSource = new PoolingDataSource(pool);
-    }
- 
 
-	
+public class ConnectionFactory {
+
+	private static interface Singleton {
+
+	}
+
+	private final DataSource dataSource;
+
+	private ConnectionFactory(String username, String passwd, String url) {
+		Properties properties = new Properties();
+		properties.setProperty("user", username);
+		properties.setProperty("password", passwd); // or get properties from
+													// some configuration file
+
+		GenericObjectPool<PoolableConnection> pool = new GenericObjectPool<PoolableConnection>();
+		DriverManagerConnectionFactory connectionFactory = new DriverManagerConnectionFactory(
+				url, properties);
+		new PoolableConnectionFactory(connectionFactory, pool, null,
+				"SELECT 1", 3, false, false,
+				Connection.TRANSACTION_READ_COMMITTED);
+
+		this.dataSource = new PoolingDataSource(pool);
+	}
+
 	public static Connection getMysqlConnection() throws SQLException {
-		String username="test";
-		String passwd="111111";
-		String url="jdbc:mysql://192.168.1.20:3306/test";
-		 final ConnectionFactory INSTANCE = new ConnectionFactory(username, passwd, url);
-        return INSTANCE.dataSource.getConnection();
-    }
-    public static Connection getOracleConnection() throws SQLException {
-    	String username="test";
-    	String passwd="111111";
-    	String url="jdbc:oracle:thin:@192.168.1.20:1521:orcl";
-    	 final ConnectionFactory INSTANCE = new ConnectionFactory(username, passwd, url);
-        return INSTANCE.dataSource.getConnection();
-    }
+		// try {
+		// // 加载MySql的驱动类
+		// Class.forName("com.mysql.jdbc.Driver");
+		// System.out.println("START GET DRIVER");
+		// } catch (ClassNotFoundException e) {
+		// System.out.println("找不到驱动程序类 ，加载驱动失败！");
+		// e.printStackTrace();
+		// }
+		String username = "test";
+		String passwd = "111111";
+		String url = "jdbc:mysql://192.168.1.20:3306/test";
+		Connection con;
+		// DriverManager.getConnection(url, username, passwd);
+		final ConnectionFactory INSTANCE = new ConnectionFactory(username,
+				passwd, url);
+		con = INSTANCE.dataSource.getConnection();
+		return con;
+	}
+
+	public static Connection getOracleConnection() throws SQLException {
+		String username = "test";
+		String passwd = "111111";
+		String url = "jdbc:oracle:thin:@192.168.1.20:1521:orcl";
+		final ConnectionFactory INSTANCE = new ConnectionFactory(username,
+				passwd, url);
+		INSTANCE.dataSource.getConnection().setAutoCommit(true);
+		return INSTANCE.dataSource.getConnection();
+	}
 }
